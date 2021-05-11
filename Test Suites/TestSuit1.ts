906d9935c4a46c00269fb087fd5bc0952ae59878
Archana Reddy <archanav@cloudscaleqa.com>
2021-05-11 19:16:08 +0530

    new test suit added

---
+++ Test Suites/TestSuit1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuit1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>045d9e22-364d-46d4-9f07-36c07ebe40ab</testSuiteGuid>
+</TestSuiteEntity>
